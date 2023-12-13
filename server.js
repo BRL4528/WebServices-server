48c400ef16255014ac156397ba41e77b7266b6fc
brl4528 <blgc.sgo@hotmail.com>
2023-12-13 19:28:07 -0300

    update all

--- server.js
+++ server.js
@@ -2,13 +2,65 @@ const WebSocket = require('ws');
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
+const { spawn  } = require("child_process");
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+let cppProcess;
+
 app.use(express.json());
 app.use(cors());
+
+
+// WebSocket
+wss.on('connection', function connection(ws) {
+  console.log('Cliente conectado!');
+
+  ws.on('message', function incoming(message) {
+    const msgString = Buffer.from(message).toString();
+    console.log('Mensagem recebida do cliente:', msgString);
+
+    if (msgString === 'roudProgram') {
+      console.log('Etapa para rodar o c++');
+      // Inicie o programa C++ como um processo separado
+      cppProcess = spawn('/home/rasp/project/darknet_test/main');
+
+      cppProcess.stdout.on('data', (data) => {
+        console.log(`Saída do programa C++: ${data}`);
+        // Aqui você pode enviar a saída para o cliente WebSocket, se necessário
+      });
+
+      cppProcess.stderr.on('data', (data) => {
+        console.error(`Erro do programa C++: ${data}`);
+          wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+              client.send('program_finalized');
+            }
+          });
+        // Aqui você pode lidar com os erros do programa C++
+      });
+
+      cppProcess.on('close', (code) => {
+        console.log(`Programa C++ encerrado com código de saída ${code}`);
+        
+          wss.clients.forEach(function each(client) {
+          
+              client.send('program_finalized');
+            
+          });
+      });
+    }
+
+    // Broadcast da mensagem recebida para todos os clientes conectados
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(msgString);
+      }
+    });
+  });
+});
 
 // Rota de autenticação básica
 app.post('/authentication', (req, res) => {
@@ -23,27 +75,24 @@ app.post('/authentication', (req, res) => {
   }
 });
 
- app.get('/teste', (req, res) => {
-    console.log('Rota GET de autenticação chamada');
-    res.status(200).json({ bateu: true });
-  });
-
-// WebSocket
-wss.on('connection', function connection(ws) {
-  console.log('Cliente conectado!');
-
-  ws.on('message', function incoming(message) {
-    const msgString = Buffer.from(message).toString();
-    console.log('Mensagem recebida do cliente:', msgString);
-
-    // Broadcast da mensagem recebida para todos os clientes conectados
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(msgString);
-      }
-    });
-  });
+app.post('/terminateProgram', (req, res) => {
+  console.log('Encerrando o programa C++');
+  terminateCppProgram(); // Função que encerra o programa C++
+  res.status(200).json({ message: 'Programa C++ encerrado' });
 });
+
+
+  const terminateCppProgram = () => {
+    // var proc = require('child_process').spawn('mongod');
+    cppProcess.kill('SIGINT');
+
+    wss.clients.forEach(function each(client) {
+          
+      client.send('program_finalized');
+    
+  });
+  };
+
 
 // Inicia o servidor
 const PORT = 3333;
