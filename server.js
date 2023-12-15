6b61c4fc075f44ce434a91cb2d67ad401510cf13
alessandro-silva <alessandro.rds03@gmail.com>
2023-12-14 22:19:13 -0400

    adding stream

--- server.js
+++ server.js
@@ -14,6 +14,90 @@ let cppProcess;
 app.use(express.json());
 app.use(cors());
 
+// STREAM
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// app.get("/video", function (req, res) {
+//   // Ensure there is a range given for the video
+//   const range = req.headers.range;
+
+//   console.log(range)
+//   if (!range) {
+//     res.status(400).send("Requires Range header");
+//   }
+
+//   const videoPath = "falador.mp4";
+//   const videoSize = fs.statSync("falador.mp4").size;
+
+//   const CHUNK_SIZE = 10 ** 6; // 1MB
+//   const start = Number(range.replace(/\D/g, ""));
+//   const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+//   // Create headers
+//   const contentLength = end - start + 1;
+//   const headers = {
+//     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+//     "Accept-Ranges": "bytes",
+//     "Content-Length": contentLength,
+//     "Content-Type": "video/mp4",
+//   };
+
+//   // HTTP Status 206 for Partial Content
+//   res.writeHead(206, headers);
+
+//   // create video read stream for this particular chunk
+//   const videoStream = fs.createReadStream(videoPath, { start, end });
+
+//   // Stream the video chunk to the client
+//   videoStream.pipe(res);
+// });
+
+// app.listen(8000, function () {
+//   console.log("Listening on port 8000!");
+// });
+
+
+//STREAM 2
+
+app.get('/video', (req, res) => {
+  const { videoPath } = req.query; // Path to your video file
+  const stat = fs.statSync(videoPath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunkSize = end - start + 1;
+    const file = fs.createReadStream(videoPath, { start, end });
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(200, head);
+    fs.createReadStream(videoPath).pipe(res);
+  }
+});
+
+// Start the server
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
+
 //APP
 app.get('/videos', (req, res) => {
   const videos = fs.readdirSync('videos');
